a5c3723146659e8f9d458d773c60b29ee45ba40c
Renan Bandeira <renanbandeira2@gmail.com>
2018-05-05 10:02:08 -0300

    Adds testers and lint

---
+++ .eslintrc.js
@@ -0,0 +1,78 @@
+module.exports = {
+  "parserOptions": {
+    "ecmaVersion": 6,
+    "ecmaFeatures": {
+      "experimentalObjectRestSpread": true,
+      "jsx": true
+    },
+    "sourceType": "module"
+  },
+
+  "env": {
+    "es6": true,
+    "node": true,
+    "browser": true,
+    "mocha": true
+  },
+
+  "plugins": [
+    "standard",
+    "promise",
+    "react",
+    "react-perf",
+    "import"
+  ],
+
+  "globals": {
+    "document": false,
+    "navigator": false,
+    "window": false,
+    "CustomElements": false,
+    "cordova": false,
+    "expect": false,
+    "mount": false,
+    "spy": false,
+    "assert": false,
+    "shallow": false,
+    "render": false
+  },
+
+  "extends": ["react-app", "airbnb", "plugin:react-perf/recommended"],
+
+  "rules": {
+    "indent": "off",
+    "no-plusplus": "off", // we want to use ++ and --, specially in for loops
+    "comma-dangle": "off", // prevent this causes a lot of useless work and git diffs
+    "react/jsx-space-before-closing": "off", // against Jetbrains defaults
+    "react/jsx-tag-spacing": "off", // effort was too high for the moment+payoff
+    "react/forbid-prop-types": "off", // effort was too high for the moment+payoff
+    "react/require-default-props": "off", // effort was too high for the moment+payoff
+    "jsx-a11y/no-static-element-interactions": "off", // we need to set onClick handlers on divs for mobile touch
+    "jsx-a11y/href-no-hash": "off",
+    "jsx-a11y/click-events-have-key-events": "off",
+    "import/prefer-default-export": "off", // webstorm doesn't detect usage of default export
+
+    "react/jsx-no-bind": ["error",
+      {
+        "ignoreRefs": false,
+        "allowArrowFunctions": false,
+        "allowBind": false
+      }
+    ], // avoid causing rerendering due to new declarations inside render
+    "max-len": ["error", 120, 2, {
+      "ignoreUrls": true,
+      "ignoreComments": false,
+      "ignoreRegExpLiterals": true,
+      "ignoreStrings": true,
+      "ignoreTemplateLiterals": true
+    }], // 120 is the Jetbrains default
+    "quote-props": ["error", "consistent-as-needed"], // Just be consistent
+    "no-param-reassign": ["error", { "props": false }], // we want to allow assigning to argument keys (reduce's accumulators)
+
+    "standard/object-curly-even-spacing": ["error", "either"],
+    "standard/array-bracket-even-spacing": ["error", "either"],
+    "standard/computed-property-even-spacing": ["error", "even"],
+
+    "promise/param-names": 2
+  }
+};
--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,32 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+const name1 = 'WTISC 2018';
+const name2 = 'ReactJS Training';
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      name: name1
+    };
+  }
+
+  changeName = () => {
+    const name = this.state.name === name1 ? name2 : name1;
+    this.setState({
+      name
+    });
+  }
+
   render() {
     return (
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">{`Welcome to ${this.state.name}`}</h1>
         </header>
+        <button onClick={this.changeName}>Change name</button>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
